ab1bc97ed2bb303b80bae414f8ea60fe5fc40683
John Wyatt <norwest1998@gmail.com>
2026-09-22 15:43:23 +1000

    added escape.html

--- shared.js
+++ shared.js
@@ -62,3 +62,8 @@
         const d = new Date(iso);
         return d.toLocaleString(undefined, { month:"short", day:"numeric", hour:"2-digit", minute:"2-digit", second:"2-digit" });
     }
+    
+    function escapeHtml(str) {
+        if (str === null || str === undefined) return '';
+        return String(str).replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/"/g,"&quot;").replace(/'/g,"&#039;");
+    }
